a08812a092f67e2eb1abfc1b1a02830a2c8507cc
Filipa Andrade <filipa.andrade@gmail.com>
2020-08-25 20:51:22 +0200

    Add logo, icon and simple menu call to backend

--- backend/api.py
+++ backend/api.py
@@ -1,11 +1,9 @@
-import time
-
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route("/menu")
+def get_menu():
+    return {"menu": ["Meetups Calendar", "Chat Bot", "Scholarships", "About us", "Vote for a topic"]}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Menu from './Menu';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Menu />
   </React.StrictMode>,
   document.getElementById('root')
 );
