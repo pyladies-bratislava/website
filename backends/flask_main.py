f42c3dc283713a969be5cd1797cb8413ba3cd4b5
Filipa Andrade <filipa.andrade@gmail.com>
2020-11-05 19:08:13 +0100

    Add chatbot skeleton with chatterbot library (#35)

--- backends/flask_main.py
+++ backends/flask_main.py
@@ -1,20 +1,32 @@
-from flask import Flask
-from .scraping import pyladies_url, get_pyladies_about_info
-from .events import SCOPES, get_google_cal_events
+from flask import Flask, request
+
+from .chatbot import bot
+from .events import get_google_cal_events
+from .scraping import get_pyladies_about_info
 
 
 app = Flask(__name__)
 
+
 @app.route("/menu")
 def get_menu():
     return {"menu": ["Meetups Calendar", "Chat Bot", "Scholarships", "About us", "Vote for a topic"]}
+
 
 @app.route("/about")
 def get_about():
     pyladies_about_info = get_pyladies_about_info()
     return {"about": "From international Pyladies website:" + pyladies_about_info}
 
+
 @app.route("/events")
 def get_events():
     events = get_google_cal_events()
     return {"events": events}
+
+
+@app.route("/chatbot", methods=["POST"])
+def post_interaction():
+    interaction = request.json["interaction"]
+    response = bot.get_response(interaction)
+    return response.text
