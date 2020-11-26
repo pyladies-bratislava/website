ff0fb74936355da3f7129897035c7d61d8a24002
Witchery <witchery.zuzka@gmail.com>
2020-11-26 19:56:59 +0100

    home page update

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,23 +1,31 @@
-import React, { Component } from "react";
+import React, {useState, useEffect} from 'react';
 import './assets/styles/Home.css'
 import './index.css'
  
-class Home extends Component {
-  render() {
+
+function Home() {
+    const [about, setAbout] = useState("About");
+
+    useEffect(() => {
+        fetch("/about").then(response => response.json()).then(data => {setAbout(data.about)})
+    }, [])
+
     return (
-      <div className="container">
-        <h2 className="header">PyLadies</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
- 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
-      </div>
-    );
-  }
+        <div className="container">
+            <p className="header">
+              PyLadies
+            </p>
+            <div className="container text-justify">
+              {about}
+            </div>
+            <div className="container">
+              <blockquote class="blockquote text-right">
+                <p class="mb-0 blockquote-main">The world needs women who code</p>
+                <footer class="blockquote-footer">Seema Saharan <cite title="Source Title">RedHat</cite></footer>
+              </blockquote>
+            </div>  
+        </div>
+  );
 }
- 
+
 export default Home;
