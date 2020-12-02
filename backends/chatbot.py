f7639cd1b2096710ecc51dcd534acf641ae76bc0
Stanislava Badurova <badurova.sta@gmail.com>
2020-12-02 20:24:51 +0100

    Change ListTrainer to ChatterBotCorpusTrainer

--- backends/chatbot.py
+++ backends/chatbot.py
@@ -1,5 +1,5 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
 
 bot = ChatBot(
@@ -12,19 +12,6 @@ bot = ChatBot(
     ],
 )
 
-trainer = ListTrainer(bot)
+trainer = ChatterBotCorpusTrainer(bot)
 
-trainer.train(
-    [
-        "Hi",
-        "Hello",
-        "I need your assistance regarding my order",
-        "Please, Provide me with your order id",
-        "I have a complaint.",
-        "Please elaborate, your concern",
-        "How long it will take to receive an order ?",
-        "An order takes 3-5 Business days to get delivered.",
-        "Okay Thanks",
-        "No Problem! Have a Good Day!",
-    ]
-)
+trainer.train('chatterbot.corpus.english')
