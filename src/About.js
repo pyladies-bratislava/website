262d8077007e1f0748309b4caaa4cda7ee4dcffc
Filipa Andrade <filipa.andrade@gmail.com>
2020-12-17 22:40:15 +0100

    Remove proxy in package.json it is only for local development

--- src/About.js
+++ src/About.js
@@ -3,9 +3,10 @@ import React, {useState, useEffect} from 'react';
 
 function About() {
     const [about, setAbout] = useState("About");
+    const api_base_url = "https://bratislava-flask-backend.herokuapp.com/about"
 
     useEffect(() => {
-        fetch("/about").then(response => response.json()).then(data => {setAbout(data.about)})
+        fetch(api_base_url).then(response => response.json()).then(data => {setAbout(data.about)})
     }, [])
 
     return (
