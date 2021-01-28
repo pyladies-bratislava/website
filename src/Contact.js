5d6573c662fd74874eee5b3fd88f8a105e24fe39
Filipa Andrade <filipa.andrade@gmail.com>
2021-01-28 19:28:32 +0100

    Add contacts

--- src/Contact.js
+++ src/Contact.js
@@ -5,6 +5,10 @@ class Contact extends Component {
         return (
             <div>
                 <h1>Contact Page</h1>
+                    <a href="https://www.facebook.com/groups/PyLadiesBratislava/">Facebook group</a>
+                    <a href="https://pyladies.slack.com/archives/CGF33FSMD">Slack</a>
+                    <a href="https://github.com/pyladies-bratislava">Github</a>
+                    <a href="mailto:bratislava@pyladies.com">Email</a>
             </div>
         );
     }
