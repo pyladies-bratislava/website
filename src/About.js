a63a7fb8fb0ea65550dd239b290e9b92cb566d2f
Filipa Andrade <filipa.andrade@gmail.com>
2021-01-28 19:06:45 +0100

    Use dotenv

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react';
 
 function About() {
     const [about, setAbout] = useState("About");
-    const api_base_url = process.env.BACKEND_URL
+    const api_base_url = process.env.REACT_APP_BACKEND_URL;
 
     useEffect(() => {
         fetch(api_base_url + "about").then(response => response.json()).then(data => {setAbout(data.about)})
