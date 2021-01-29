b852834c890123f3404fd261318216dd52cb0846
Stanka-ai <61557381+Stanka-ai@users.noreply.github.com>
2021-01-29 07:13:43 +0100

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -6,8 +6,11 @@ class Contact extends Component {
             <div>
                 <h1>Contact Page</h1>
                     <a href="https://www.facebook.com/groups/PyLadiesBratislava/">Facebook group</a>
+                    <br/>
                     <a href="https://pyladies.slack.com/archives/CGF33FSMD">Slack</a>
+                    <br/>
                     <a href="https://github.com/pyladies-bratislava">Github</a>
+                    <br/>
                     <a href="mailto:bratislava@pyladies.com">Email</a>
             </div>
         );
