06edbe6995ead8a1ff9165acd7e226e7bf0ac606
Witchery <witchery.zuzka@gmail.com>
2021-01-30 14:12:38 +0100

    updated contact page with links/icons

--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import './Contact.css';
 
 class Contact extends Component {
     render () {
         return (
-            <div>
-                <h1>Contact Page</h1>
+            <React.Fragment> 
+            <div class="container">
+                <img src="/assets/jumbotron.jpeg" class="img-fluid" alt="Responsive image" width="1150" height="350"></img>
             </div>
+            <div class="container">    
+                    <h1 class="text-center">We'd <i class="far fa-heart"></i> to help.</h1>
+                    <h4 class="text-center">We like to create things with fun, open minded people. <br /> Feel free to say hello.</h4>
+                <div class="row">
+                    <div class="col-3">
+                        <div class="card border-0">
+                            <div class="card-body text-center">
+                                <a class="text-dark" href="https://www.facebook.com/groups/PyLadiesBratislava/" target="_blank" rel="noopener"><i class="fab fa-facebook fa-4x" aria-hidden="true"></i></a>
+                                <p class="card-text">Facebook</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card border-0">
+                            <div class="card-body text-center">
+                                <a class="text-dark" href="https://pyladies.slack.com/archives/CGF33FSMD" target="_blank" rel="noopener"><i class="fab fa-slack fa-4x" aria-hidden="true"></i></a>
+                                <p class="card-text">Slack</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card border-0">
+                            <div class="card-body text-center">
+                                <a class="text-dark" href="https://github.com/pyladies-bratislava" target="_blank" rel="noopener"><i class="fab fa-github fa-4x" aria-hidden="true"></i></a>
+                                <p class="card-text">GitHub</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="card border-0">
+                            <div class="card-body text-center">
+                                <a class="text-dark" href="mailto:bratislava@pyladies.com" target="_blank" rel="noopener"><i class="fas fa-at fa-4x" aria-hidden="true"></i></a>
+                                <p class="card-text">Email</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>    
         );
     }
 }
