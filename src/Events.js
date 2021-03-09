a0dc2fbbf293d95fb6b9dce8653cd025ffe9a55b
Filipa Andrade <filipa.andrade@gmail.com>
2021-03-09 12:24:06 +0100

    Add events api call

--- src/Events.js
+++ src/Events.js
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import './menu.css'
+
 
 function Events() {
     const [events, setEvents] = useState("Events");
+    const api_base_url = process.env.REACT_APP_BACKEND_URL;
 
     useEffect(() => {
-        fetch("/events").then(response => response.json()).then(data => {setEvents(data.events)})
+        fetch(api_base_url + "events").then(response => response.json()).then(data => {setEvents(data.events)})
     }, [])
 
     return (
