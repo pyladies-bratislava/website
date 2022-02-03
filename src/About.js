741747f1de717a747790d4556abd723278252a7b
Irina Baeva <irina.baeva@bonial.com>
2022-02-03 21:03:31 +0100

    Add eslint/prettier and apply

--- src/About.js
+++ src/About.js
@@ -1,48 +1,44 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect } from "react";
 
 function About() {
-    const [about, setAbout] = useState([]);
-    const [fetching, setFetching] = useState(false)
-    const [error, setError] = useState("")
-    const api_base_url = process.env.REACT_APP_BACKEND_URL;
+  const [about, setAbout] = useState([]);
+  const [fetching, setFetching] = useState(false);
+  const [error, setError] = useState("");
+  const api_base_url = process.env.REACT_APP_BACKEND_URL;
 
-    useEffect(() => {
-        fetchAboutData()
-    }, [])
+  useEffect(() => {
+    fetchAboutData();
+  }, []);
 
-    const fetchAboutData = async () => {
-        setFetching(true)
-        try {
-            const response = await fetch(api_base_url + "about")
-            const data = await response.json()
-            setAbout(data.about)
-            setFetching(false)
-        }
-        catch (e) {
-            setFetching(false)
-            setError(e)
-        }
+  const fetchAboutData = async () => {
+    setFetching(true);
+    try {
+      const response = await fetch(`${api_base_url}about`);
+      const data = await response.json();
+      setAbout(data.about);
+      setFetching(false);
+    } catch (e) {
+      setFetching(false);
+      setError(e);
     }
+  };
 
-    return (
-        <div>
-            {
-                fetching ? (
-                    <div class="spinner-border text-info" role="status">
-                        <span class="visually-hidden">Loading...</span>
-                    </div>
-                ) : about
-            }
-            {
-                error && (
-                    <div class="alert alert-danger" role="alert">
-                        {error.message}
-                    </div>
-                )
-            }
+  return (
+    <div>
+      {fetching ? (
+        <div className="spinner-border text-info" role="status">
+          <span className="visually-hidden">Loading...</span>
         </div>
-    );
+      ) : (
+        about
+      )}
+      {error && (
+        <div className="alert alert-danger" role="alert">
+          {error.message}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default About;
--- src/Events.js
+++ src/Events.js
@@ -1,52 +1,49 @@
-import React, {useState, useEffect} from 'react';
-import './menu.css'
+import React, { useState, useEffect } from "react";
+import "./menu.css";
 
 function Events() {
-    const [events, setEvents] = useState([]);
-    const [fetching, setFetching] = useState(false)
-    const [error, setError] = useState("")
-    const api_base_url = process.env.REACT_APP_BACKEND_URL;
+  const [events, setEvents] = useState([]);
+  const [fetching, setFetching] = useState(false);
+  const [error, setError] = useState("");
+  const api_base_url = process.env.REACT_APP_BACKEND_URL;
 
-    useEffect(() => {
-        fetchAboutData()
-    }, [])
+  useEffect(() => {
+    fetchAboutData();
+  }, []);
 
-    const fetchAboutData = async () => {
-        setFetching(true)
-        try {
-            const response = await fetch(api_base_url + "events")
-            const { events } = await response.json()
-            if (events.length === 0) {
-                setEvents("No events found")
-            } else {
-                setEvents(events)
-            }
-            setFetching(false)
-        }
-        catch (e) {
-            setFetching(false)
-            setError(e)
-        }
+  const fetchAboutData = async () => {
+    setFetching(true);
+    try {
+      const response = await fetch(`${api_base_url}events`);
+      const { events } = await response.json();
+      if (events.length === 0) {
+        setEvents("No events found");
+      } else {
+        setEvents(events);
+      }
+      setFetching(false);
+    } catch (e) {
+      setFetching(false);
+      setError(e);
     }
+  };
 
-    return (
-        <div>
-            {
-                fetching ? (
-                    <div class="spinner-border text-info" role="status">
-                        <span class="visually-hidden">Loading...</span>
-                    </div>
-                ) : events
-            }
-            {
-                error && (
-                    <div class="alert alert-danger" role="alert">
-                        {error.message}
-                    </div>
-                )
-            }
+  return (
+    <div>
+      {fetching ? (
+        <div className="spinner-border text-info" role="status">
+          <span className="visually-hidden">Loading...</span>
         </div>
-    );
+      ) : (
+        events
+      )}
+      {error && (
+        <div className="alert alert-danger" role="alert">
+          {error.message}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Events;
