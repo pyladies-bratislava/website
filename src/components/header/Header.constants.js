bd20746b105f02c9087d154b939e073611ff0554
Irina Baeva <irina.baeva@bonial.com>
2022-09-13 15:27:44 +0200

    Make header functional component

---
+++ src/components/header/Header.constants.js
@@ -0,0 +1,7 @@
+export const MENU_LINKS = [
+  { path: "/About", text: "About Us", isActive: false },
+  { path: "/MeetUps", text: "MeetUps", isActive: false },
+  { path: "/Events", text: "Events", isActive: false },
+  { path: "/3", text: "Stories", isActive: false },
+  { path: "/Contact", text: "Contact", isActive: false },
+];
