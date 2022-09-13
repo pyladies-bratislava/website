47965461bf5622f07c879797fd0bcd7fff5c41e8
Irina Baeva <irina.baeva@bonial.com>
2022-09-13 14:26:21 +0200

    Refactor contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,101 +1,89 @@
 import React from "react";
+import { Container, Image, Row, Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { ButtonMailTo } from "../components/button-mail-to";
+
+const SOCIAL_MEDIA_CONFIG = [
+  {
+    name: "Facebook",
+    icon: <i className="fab fa-facebook fa-4x" aria-hidden="true" />,
+    pathname: "https://www.facebook.com/groups/PyLadiesBratislava/",
+  },
+  {
+    name: "Slack",
+    icon: <i className="fab fa-slack fa-4x" aria-hidden="true" />,
+    pathname: "https://pyladies.slack.com/archives/CGF33FSMD",
+  },
+  {
+    name: "GitHub",
+    icon: <i className="fab fa-github fa-4x" aria-hidden="true" />,
+    pathname: "https://github.com/pyladies-bratislava",
+  },
+  {
+    name: "Email",
+    icon: <i className="fas fa-at fa-4x" aria-hidden="true" />,
+    pathname: "#",
+    mailto: "mailto:bratislava@pyladies.com",
+  },
+];
 
 export const Contact = () => {
   return (
     <>
-      <div className="container">
-        <img
+      <Container className="container">
+        <Image
           src="/assets/jumbotron.jpeg"
-          className="img-fluid"
+          fluid
           alt="Responsive"
           width="1150"
           height="350"
         />
-      </div>
-      <div className="container">
-        <h1 className="text-center">
+      </Container>
+      <Container>
+        <h2 className="text-center">
           We&apos;d <i className="far fa-heart" /> to help.
-        </h1>
-        <h4 className="text-center">
+        </h2>
+        <h5 className="text-center">
           We like to create things with fun, open minded people. <br /> Feel
           free to say hello.
-        </h4>
-        <div className="row">
-          <div className="col-3">
-            <div className="card border-0">
-              <div className="card-body text-center">
-                <Link
-                  className="text-dark"
-                  to={{
-                    pathname:
-                      "https://www.facebook.com/groups/PyLadiesBratislava/",
-                  }}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fab fa-facebook fa-4x" aria-hidden="true" />
-                </Link>
-                <p className="card-text">Facebook</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-3">
-            <div className="card border-0">
-              <div className="card-body text-center">
-                <Link
-                  className="text-dark"
-                  to={{
-                    pathname: "https://pyladies.slack.com/archives/CGF33FSMD",
-                  }}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fab fa-slack fa-4x" aria-hidden="true" />
-                </Link>
-                <p className="card-text">Slack</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-3">
-            <div className="card border-0">
-              <div className="card-body text-center">
-                <Link
-                  className="text-dark"
-                  to={{ pathname: "https://github.com/pyladies-bratislava" }}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className="fab fa-github fa-4x" aria-hidden="true" />
-                </Link>
-                <p className="card-text">GitHub</p>
-              </div>
-            </div>
-          </div>
-          <div className="col-3">
-            <div className="card border-0">
-              <div className="card-body text-center">
-                <ButtonMailTo
-                  mailto="mailto:bratislava@pyladies.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  label="labelWithIcon"
-                />
-                <p className="card-text">Email</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="container text-center pt-5 pb-5">
-        <h3 className="text-decorated">
+        </h5>
+        <Row>
+          {SOCIAL_MEDIA_CONFIG.map((item) => {
+            return (
+              <Col key={item.name}>
+                <Card border="0">
+                  <Card.Body className="text-center">
+                    <Link
+                      className="text-dark"
+                      to={{
+                        pathname: item.pathname,
+                      }}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      onClick={(e) => {
+                        if (item.name === "Email") {
+                          window.location = item.mailto;
+                          e.preventDefault();
+                        }
+                      }}
+                    >
+                      {item.icon}
+                    </Link>
+                    <Card.Text className="card-text">{item.name}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+      <Container className="container text-center pt-5 pb-5">
+        <h4 className="text-decorated">
           We are here to help you start learnig coding. Send us an email at
-        </h3>
-        <h2 className="text-decorated font-weight-bold">
+        </h4>
+        <h3 className="text-decorated font-weight-bold">
           bratislava@pyladies.com
-        </h2>
-      </div>
+        </h3>
+      </Container>
     </>
   );
 };
