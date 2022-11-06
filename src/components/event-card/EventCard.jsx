f0a2f64c0bd62986bee44bfcbe241e0b861ba58b
Irina Baeva <irina.baeva@bonial.com>
2022-11-06 19:40:48 +0100

    Implement event page

---
+++ src/components/event-card/EventCard.jsx
@@ -0,0 +1,18 @@
+import Card from "react-bootstrap/Card";
+import logo from "../../assets/pyladies-fb.jpeg";
+import { formatDate } from "../../utils";
+
+export const EventCard = ({ title, startDate, description }) => {
+  return (
+    <Card style={{ width: "20rem" }}>
+      <Card.Img variant="top" src={logo} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Subtitle className="mb-2 mt-2 text-muted">
+          {formatDate(startDate)}
+        </Card.Subtitle>
+        <Card.Text>{description}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,23 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { Container, Alert } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
 
 import { API_BASE_URL } from "../constants";
 import { Loader } from "../components/loader/Loader";
+import { EventCard } from "../components/event-card"
+import { sortArrayByDate } from "../utils";
 
 export const Events = () => {
   const [listOfEvents, setListOfEvents] = useState([]);
   const [fetching, setFetching] = useState(false);
   const [error, setError] = useState("");
 
+  const isNoData = !fetching && listOfEvents.length === 0;
   const fetchEventsData = async () => {
     setFetching(true);
     try {
       const response = await fetch(`${API_BASE_URL}events`);
       const { events } = await response.json();
       if (events.length === 0) {
-        setListOfEvents("No events found");
+        setListOfEvents([]);
       } else {
-        setListOfEvents(events);
+        const sortedByDateArray = sortArrayByDate(events);
+        setListOfEvents(sortedByDateArray);
       }
       setFetching(false);
     } catch (e) {
@@ -32,11 +39,22 @@ export const Events = () => {
 
   return (
     <Container>
-      {fetching ? (
-        <Loader />
-      ) : (
-        JSON.stringify(listOfEvents) // temporary
-      )}
+      {fetching && <Loader />}
+      {isNoData && <div>No Events</div>}
+      <Row xs={1} md={3} className="g-4">
+        {listOfEvents.length > 0 &&
+          listOfEvents.map((event) => {
+            return (
+              <Col key={event.created}>
+                <EventCard
+                  title={event.summary}
+                  description={event.description}
+                  startDate={event.start.dateTime}
+                />
+              </Col>
+            );
+          })}
+      </Row>
       {error && <Alert variant="danger">{error.message}</Alert>}
     </Container>
   );
