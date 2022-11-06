bcc552b88cbed5ef0061216a5c8adee811c59b95
Irina Baeva <irina.baeva@bonial.com>
2022-11-06 20:27:47 +0100

    Fix header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,7 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 import { MENU_LINKS } from "./Header.constants";
 import { NavLink } from "../nav-link/NavLink";
 import { Logo } from "../logo";
@@ -10,33 +14,26 @@ export const Header = () => {
   };
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark  bg-dark">
-        <Logo />
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbar-toggle"
-          aria-controls="nav-content"
-          aria-expanded="false"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
+      <Navbar expand="lg" bg="dark" className="navbar-dark">
+        <Container>
+          <Logo />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-        <div className="collapse navbar-collapse" id="navbar-toggle">
-          <ul className="navbar-nav mr-auto">
-            {MENU_LINKS.map((link, index) => (
-              <NavLink
-                path={link.path}
-                text={link.text}
-                isActive={link.isActive}
-                key={link.path}
-                onClick={(_) => handleClick(_, index)}
-              />
-            ))}
-          </ul>
-        </div>
-      </nav>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              {MENU_LINKS.map((link, index) => (
+                <NavLink
+                  path={link.path}
+                  text={link.text}
+                  isActive={link.isActive}
+                  key={link.path}
+                  onClick={(_) => handleClick(_, index)}
+                />
+              ))}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
